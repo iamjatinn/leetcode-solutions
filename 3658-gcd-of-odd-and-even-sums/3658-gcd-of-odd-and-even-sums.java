ac61b5e5d2f7073ba0956e2f7905a4f5a18637f4
Jatin Sachdev <jatinsachdv@gmail.com>
2026-07-15 21:47:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (6.12%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -8,13 +8,15 @@ class Solution {
         //     sumEven += 2 * i;
         // }
 
-        return computeGcd(n*(n+1), n*n);
+    //     return computeGcd(n*(n+1), n*n);
 
+    // }
+    //     private static int computeGcd(int a, int b){
+    //         if(b == 0)  return a;
+    //         else{
+    //             return computeGcd(b, a%b);
+    //         }
+    //     }
+    return n;
     }
-        private static int computeGcd(int a, int b){
-            if(b == 0)  return a;
-            else{
-                return computeGcd(b, a%b);
-            }
-        }
 }
