afa2d90304ed4752bb6abfa0c50aa2dd0013833d
Jatin Sachdev <jatinsachdv@gmail.com>
2026-07-20 19:37:37 +0530

    Time: 6 ms (55.4%), Space: 47 MB (79.54%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -2,36 +2,37 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-
-        int total = m * n;
-        k %= total;
-
-        List<List<Integer>> ans = new ArrayList<>();
-
+        
+        int q = k / n;
+        int r = k % n;
+        q %= m;
+        
+        int[][] ans = new int[m][n];
+        
         for (int i = 0; i < m; i++) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < n; j++)
-                row.add(0);
-            ans.add(row);
-        }
-
-        for (int i = 0; i < m; i++) {
+            int i1 = i + q + 1;
             for (int j = 0; j < n; j++) {
-
-                // index in 1D array (before rotation)
-                int oldIndex = i * n + j;
-
-                // index in 1D array (after rotation)
-                int newIndex = (oldIndex + k) % total;
-
-                // changing from 1D back to 2D
-                int newRow = newIndex / n;
-                int newCol = newIndex % n;
-
-                ans.get(newRow).set(newCol, grid[i][j]);
+                boolean minus1 = (j + r < n);
+                int j0 = j + r;
+                j0 -= (j0 >= n) ? n : 0;
+                
+                int i0 = i1 - (minus1 ? 1 : 0);
+                i0 -= (i0 >= m) ? m : 0;
+                
+                ans[i0][j0] = grid[i][j];
             }
         }
 
-        return ans;
+        // Fixed return type conversion
+        List<List<Integer>> result = new ArrayList<>();
+        for (int[] row : ans) {
+            List<Integer> listRow = new ArrayList<>();
+            for (int val : row) {
+                listRow.add(val);
+            }
+            result.add(listRow);
+        }
+        
+        return result;
     }
 }
